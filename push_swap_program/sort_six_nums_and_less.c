4c54f18962fb5e475b81a58935a32b3622605114
iltafah <eliastafah@gmail.com>
2021-09-20 16:48:04 +0100

    I was so stupid to forget this condition

--- push_swap_program/sort_six_nums_and_less.c
+++ push_swap_program/sort_six_nums_and_less.c
@@ -6,7 +6,7 @@
 /*   By: iltafah <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/18 11:23:09 by iltafah           #+#    #+#             */
-/*   Updated: 2021/09/18 13:10:49 by iltafah          ###   ########.fr       */
+/*   Updated: 2021/09/20 16:45:41 by iltafah          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,18 @@
 
 void	sort_six_nums_and_less(t_ps_vars *vars)
 {
-	while (vars->stack_a.used_size > 3)
+	int	min_num;
+
+	min_num = get_min_num(vars->stack_a);
+	if (is_it_sorted_circularly(vars->stack_a, min_num) == false)
 	{
-		push_from_to(&vars->stack_a, &vars->stack_b);
-		ft_putstr_fd("pb\n", STDOUT_FILENO);
+		while (vars->stack_a.used_size > 3)
+		{
+			push_from_to(&vars->stack_a, &vars->stack_b);
+			ft_putstr_fd("pb\n", STDOUT_FILENO);
+		}
+		swap_and_sort(&vars->stack_a);
+		retrieve_nums_from_b_in_perfect_spot(vars);
 	}
-	swap_and_sort(&vars->stack_a);
-	retrieve_nums_from_b_in_perfect_spot(vars);
 	sort_stack_a(&vars->stack_a);
 }
